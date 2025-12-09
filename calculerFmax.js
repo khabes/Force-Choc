f6b1d5293d3d82f7f3ddfd835c6ab950bbe55812
Khabes <khabes@gmail.com>
2025-12-09 10:23:10 +0100

    create icon folders and add images

--- calculerFmax.js
+++ calculerFmax.js
@@ -51,9 +51,9 @@ document.getElementById('calcForm').addEventListener('submit', function(event) {
     const facteur_choc = (Fmax / P).toLocaleString('fr-FR', { maximumFractionDigits: 2 });
     
     resultatDiv.innerHTML = `
-        <p><img src="Weight.png" alt="Poids statique" height="24" width="24"> Poids statique (P) : ${P_text} N</p>
-        <p><img src="icon-192x192.png" alt="Force d'Impact maximale" height="24" width="24"> Force d'Impact maximale (F<sub>max</sub>) : ${Fmax_text} N</p>
-        <p><img src="Calc.png" alt="Facteur de Choc" height="24" width="24"> Facteur de Choc (F<sub>max</sub> / P) : ${facteur_choc}</p>
+        <p><img src="icon/Weight.png" alt="Poids statique" height="24" width="24"> Poids statique (P) : ${P_text} N</p>
+        <p><img src="icon/icon-192x192.png" alt="Force d'Impact maximale" height="24" width="24"> Force d'Impact maximale (F<sub>max</sub>) : ${Fmax_text} N</p>
+        <p><img src="icon/Calc.png" alt="Facteur de Choc" height="24" width="24"> Facteur de Choc (F<sub>max</sub> / P) : ${facteur_choc}</p>
     `;
     resultatDiv.style.display = 'block';
 });
